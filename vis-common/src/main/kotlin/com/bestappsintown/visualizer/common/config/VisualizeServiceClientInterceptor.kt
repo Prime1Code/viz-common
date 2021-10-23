f7de2356edfe593e9a2d04ed9f43d53d56fb09f4
Prime1Code <maxi.jaku@gmail.com>
2021-10-23 14:14:44 +0200

    exception handling and dto update

--- vis-common/src/main/kotlin/com/bestappsintown/visualizer/common/config/VisualizeServiceClientInterceptor.kt
+++ vis-common/src/main/kotlin/com/bestappsintown/visualizer/common/config/VisualizeServiceClientInterceptor.kt
@@ -8,6 +8,7 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.getBeansWithAnnotation
 import org.springframework.context.ApplicationContext
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
 import java.io.IOException
@@ -22,55 +23,61 @@ class VisualizeServiceClientInterceptor @Autowired constructor(private val appli
     private val client = OkHttpClient()
 
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
-        val method = handler as HandlerMethod
-        var msPort: String? = request.getHeader("MS-Port")
-        var msName: String? = request.getHeader("MS-Name")
-        var applicationName: String = applicationContext.applicationName;
+        try {
+            val method = handler as HandlerMethod
+            var msPort: String? = request.getHeader("MS-Port")
+            var msName: String? = request.getHeader("MS-Name")
+            var applicationName: String = applicationContext.applicationName;
 
-        if (applicationName.isEmpty()) {
-            val classes: Map<String, Any> = applicationContext.getBeansWithAnnotation<VisualizeServiceClient>()
-            val mainClass: Map.Entry<String, Any> = classes.entries.stream().findFirst().get()
-            val visualizeServiceClientAnnotation: VisualizeServiceClient =
-                mainClass.value.javaClass.annotations.filter { t -> t.annotationClass == VisualizeServiceClient::class }
-                    .first() as VisualizeServiceClient
-            applicationName = visualizeServiceClientAnnotation.applicationName
-        }
-
-        val ping: Ping = Ping(
-            msName,
-            request.remoteHost,
-            request.remoteAddr,
-            msPort ?: request.remotePort.toString(),
-            applicationName,
-            request.localName,
-            request.localAddr,
-            request.localPort.toString(),
-            URI.create(request.requestURI),
-            method.method.name,
-            Date()
-        )
-
-        val request = Request.Builder()
-            .url("http://localhost:8100/ping/")
-            .post(jacksonObjectMapper().writeValueAsString(ping).toRequestBody(MEDIA_TYPE_JSON))
-            .build()
-
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                e.printStackTrace()
+            if (applicationName.isEmpty()) {
+                val classes: Map<String, Any> = applicationContext.getBeansWithAnnotation<VisualizeServiceClient>()
+                val mainClass: Map.Entry<String, Any> = classes.entries.stream().findFirst().get()
+                val visualizeServiceClientAnnotation: VisualizeServiceClient =
+                    mainClass.value.javaClass.annotations.filter { t -> t.annotationClass == VisualizeServiceClient::class }
+                        .first() as VisualizeServiceClient
+                applicationName = visualizeServiceClientAnnotation.applicationName
             }
 
-            override fun onResponse(call: Call, response: Response) {
-                response.use {
-                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            val ping = Ping(
+                msName,
+                request.remoteHost,
+                request.remoteAddr,
+                msPort ?: request.remotePort.toString(),
+                applicationName,
+                request.localName,
+                request.localAddr,
+                request.localPort.toString(),
+                URI.create(request.requestURI),
+                method.method.name,
+                Date(),
+                response.status.toString(),
+            )
+
+            val jsonBody = jacksonObjectMapper().writeValueAsString(ping)
+
+            val request = Request.Builder()
+                .url("http://localhost:8100/ping/")
+                .post(jsonBody.toRequestBody(MEDIA_TYPE_JSON))
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
                 }
-            }
-        })
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+                    }
+                }
+            })
+        } catch (e: ClassCastException) {
+        }
 
         return true;
     }
 
     companion object {
-        val MEDIA_TYPE_JSON = org.springframework.http.MediaType.APPLICATION_JSON_VALUE.toMediaType()
+        val MEDIA_TYPE_JSON = APPLICATION_JSON_VALUE.toMediaType()
     }
 }
--- vis-common/src/main/kotlin/com/bestappsintown/visualizer/common/dto/Ping.kt
+++ vis-common/src/main/kotlin/com/bestappsintown/visualizer/common/dto/Ping.kt
@@ -4,7 +4,18 @@ import java.net.URI
 import java.util.*
 
 data class Ping(
-    val srcServiceName: String?, val srcHostName: String, val srcHostAddr: String, val srcHostPort: String,
-    val destServiceName: String, val destHostName: String, val destHostAddr: String, val destHostPort: String,
-    val destUri: URI, val destEndpointMethod: String, val timestamp: Date
+    val srcServiceName: String?,
+    val srcHostName: String,
+    val srcHostAddr: String,
+    val srcHostPort: String,
+
+    val destServiceName: String,
+    val destHostName: String,
+    val destHostAddr: String,
+    val destHostPort: String,
+    val destUri: URI,
+    val destEndpointMethod: String,
+
+    val timestamp: Date,
+    val responseStatus: String,
 )
