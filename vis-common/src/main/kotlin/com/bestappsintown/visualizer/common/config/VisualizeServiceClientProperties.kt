8bec38e1bf9f135aa5aa539b661b07de3c065efe
Prime1Code <maxi.jaku@gmail.com>
2021-11-07 19:58:24 +0100

    renaming

--- vis-common/src/main/kotlin/com/bestappsintown/visualizer/common/config/VisualizeServiceClientProperties.kt
+++
@@ -1,9 +0,0 @@
-package com.bestappsintown.visualizer.common.config
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties("vsc")
-class VisualizeServiceClientProperties {
-    private lateinit var vssPort: String
-    private lateinit var vssUrl: String
-}
